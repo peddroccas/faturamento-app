e944bddd909938ed172e547245a9e72e15d01263
Pedro Bernardes <pedroabernardes11@gmail.com>
2024-05-20 12:20:39 -0300

    feat: create page skeleton

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,15 +1,25 @@
-import { yearData } from "../services/api";
-export function BasicTable() {
-  const rows = yearData
+interface TableProps {
+  headers: string[];
+  rows: number;
+}
 
-
-
+export function Table({ headers, rows }: TableProps) {
   return (
-    <div className="p-4">
-      <table>
-        <thead></thead>
-        <tbody>
-
+    <div className="my-4 p-2 border border-aliceblue rounded">
+      <table className="">
+        <thead>
+          <tr>
+            {headers.map((header) => (
+              <th className="p-4" id={header}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="text-center border-t border-l-4 rounded-l">
+          <tr className="">
+            <td className="leading-4 p-2 bg-bluesr-400  text-aliceblue">{[30, 1, 2]}</td>
+            <td className="leading-4 p-2 bg-bluesr-400  text-aliceblue">{"30%"}</td>
+            <td className="leading-4 p-2 bg-bluesr-400  text-aliceblue">{"10%"}</td>
+          </tr>
         </tbody>
       </table>
     </div>
--- src/pages/Faturamento.tsx
+++ src/pages/Faturamento.tsx
@@ -1,11 +1,25 @@
-import { BasicTable } from "../components/Table";
-
+import { Table } from "../components/Table";
+import { yearData } from "../services/api";
 
 export function Faturamento() {
+  const rows = yearData;
   return (
-    <div className="h-screen w-auto p-6 flex flex-col items-center gap-4">
-      <h1 className="text-4xl mb-2">Faturamento</h1>
-      <BasicTable></BasicTable>
+    <div className="flex-1 h-screen w-auto flex flex-col">
+      <header className="flex gap-4 p-4 border-b border-b-aliceblue">
+        <h1 className="text-3xl">Faturamento</h1>
+        <select className="text-bluesr-500 rounded">
+          <option value="draven">2017</option>
+          <option value="iei">2018</option>
+        </select>
+      </header>
+      <main className="m-4 gap-2 text-bluesr-500">
+        <section className="w-fit bg-aliceblue rounded">
+          <Table headers={['Valor', 'Mês passado', 'Ano passado']} rows={2}></Table>
+        </section>
+        <section className="w-full  bg-aliceblue rounded">
+          <p>helloword</p>
+        </section>
+      </main>
     </div>
   );
 }
