bf9eba7c25473a53087776db246a343b7e22bf6b
Pedro Antunes <pedro@ctir.gov.br>
2024-07-25 15:17:18 -0300

    bug: fix firebase bug

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,12 +3,12 @@ import 'firebase/compat/auth' // se você estiver usando autenticação
 import 'firebase/compat/database' // se você estiver usando o Firestore
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
+  apiKey: process.env.ApiKey,
+  authDomain: process.env.AuthDomain,
+  projectId: process.env.ProjectId,
+  storageBucket: process.env.StorageBucket,
+  messagingSenderId: process.env.MessagingSenderId,
+  appId: process.env.AppId,
 }
 
 // Inicializar o Firebase
